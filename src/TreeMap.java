92a705cb534a1da6ebb45352354254a893a9a164
Pierce Lamb <richard.pierce.lamb@gmail.com>
2014-10-21 15:48:15 -0700

    All initial files

---
+++ src/TreeMap.java
@@ -0,0 +1,29 @@
+// TODO: Remove each 'todo' comment once I implement each part!
+
+// TODO: class comment header
+
+public class TreeMap<K extends Comparable<K>, V> extends AbstractTreeMap<K, V> {
+	
+	public TreeMap() {
+		// TODO: implement the constructor
+		
+	}
+		
+	// TODO: comment header
+	public String toString() {
+		// TODO: implement this method
+		return "TODO: implement toString";
+	}
+	
+	// TODO: comment header
+	protected TreeNode put(TreeNode node, K key, V value) {
+		// TODO: implement this method
+		return super.put(node, key, value);
+	}
+	
+	// TODO: comment header
+	protected TreeNode remove(TreeNode node, K key) {
+		// TODO: implement this method
+		return super.remove(node, key);
+	}
+}
---
+++ src/TreeSet.java
@@ -0,0 +1,56 @@
+// TODO: Remove each 'todo' comment once I implement each part!
+
+// TODO: class comment header
+
+import java.util.Iterator;
+
+public class TreeSet<E extends Comparable<E>> implements Set<E> {
+	// TODO: declare my private fields here
+	
+	public TreeSet() {
+		// TODO: implement the constructor
+		
+	}
+
+	// TODO: comment header
+	public void add(E element) {
+		// TODO: implement this method
+		
+	}
+
+	// TODO: comment header
+	public boolean contains(E element) {
+		// TODO: implement this method
+		return false;
+	}
+
+	// TODO: comment header
+	public boolean isEmpty() {
+		// TODO: implement this method
+		return false;
+	}
+
+	// TODO: comment header
+	public Iterator<E> iterator() {
+		// TODO: implement this method
+		return null;
+	}
+
+	// TODO: comment header
+	public void remove(E element) {
+		// TODO: implement this method
+		
+	}
+
+	// TODO: comment header
+	public int size() {
+		// TODO: implement this method
+		return 0;
+	}
+	
+	// TODO: comment header
+	public String toString() {
+		// TODO: implement this method
+		return "TODO: implement toString";
+	}
+}
